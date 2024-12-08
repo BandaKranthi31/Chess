c584fc314074a3122ef9a58e4d795f9e187160e8
Kranthi <22wj1a6610@gniindia.org>
2024-12-08 11:46:27 +0530

    added white pieces

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+
+
 public class Main{
 
     public static void main(String[] args) {
+
+
         Cell[][] board = new Cell[8][8];
      
         for (int col = 0; col < 8; col++){
@@ -20,13 +24,42 @@ public class Main{
         board[0][6] = new Cell(new Knight(0, 6, 'B'));
         board[0][7] = new Cell(new Rook(0, 7, 'B'));
 
+
+        board[7][0] = new Cell(new Rook(0, 0, 'W'));
+        board[7][1] = new Cell(new Knight(0, 1, 'W'));
+        board[7][2] = new Cell(new Bishop(0, 2, 'W'));
+        board[7][3] = new Cell(new Queen(0, 3, 'W'));
+        board[7][4] = new Cell(new King(0, 4, 'W'));
+        board[7][5] = new Cell(new Bishop(0, 5, 'W'));
+        board[7][6] = new Cell(new Knight(0, 6, 'W'));
+        board[7][7] = new Cell(new Rook(0, 7, 'W'));
+
+
+
+
         
 
-        // for (int i = 0; i < board.length; i++) {
-        //     for (int j = 0; j < board.length; j++) {
-        //         System.out.print(board[i][j]);
-        //     }
-        //     System.out.println();
-        // }
+           
+
+
+        for (Cell[] board1 : board) {
+                    for (int j = 0; j < board.length; j++) {
+                        System.out.print(board1[j]);
+                    }
+                    System.out.println();
+                }
+            
+        // printBoard(board[][]);
     }
+
+    // private  void printBoard(new board[]){
+        
+        
+    //     for (Cell[] board1 : board) {
+    //         for (int j = 0; j < board.length; j++) {
+    //             System.out.print(board1[j]);
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 }
