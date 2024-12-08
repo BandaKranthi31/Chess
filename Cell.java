de562e8f89475c57bd894763bec4e2509781ad3d
Kranthi <22wj1a6610@gniindia.org>
2024-12-08 11:00:11 +0530

    "started Project"

---
+++ Cell.java
@@ -0,0 +1,10 @@
+public class Cell {
+    Piece p1 = null;
+
+    public Cell(Piece p1) {
+       this.p1 = p1;
+    }
+    
+
+}
+ 
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+
+    public static void main(String[] args) {
+        Cell[][] board = new Cell[8][8];
+        
+    }
+}
